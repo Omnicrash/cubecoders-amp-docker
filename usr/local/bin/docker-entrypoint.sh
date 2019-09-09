57d5fba670d51a298e3ed54b61083d6e61badc10
Yve Verstrepen <yve@omnicrash.net>
2019-09-09 04:08:43 +0200

    Fix: Usermod params

--- usr/local/bin/docker-entrypoint.sh
+++ usr/local/bin/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Update user IDs
-usermod -u ${PUID} -g ${PGPID} amp 2>/dev/null
+usermod -u ${PUID} -g ${PGID} amp 2>/dev/null
 
 exec su -l amp -c "AMPUSER=$AMPUSER AMPPASSWORD=$AMPPASSWORD BINDADDRESS=$BINDADDRESS PORT=$PORT $@"
